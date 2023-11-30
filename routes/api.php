30f882805ace10cc9be0163d6d67e5cca8b55b85
DMITRII1548 <korsunov2006@list.ru>
2023-11-30 20:43:23 +0600

    added and updated files

--- routes/api.php
+++ routes/api.php
@@ -105,7 +105,6 @@ Route::group([
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class ,'me']);
-
 });
 
 Route::post('register', [\App\Http\Controllers\RegisterController::class, 'register']);
